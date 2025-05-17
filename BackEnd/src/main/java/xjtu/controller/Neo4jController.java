1b72230ae56845022269da8b75da8b8863cf996d
张先生 <15091076006@163.com>
2025-05-17 15:06:41 +0800

    修改接口名称

--- BackEnd/src/main/java/xjtu/controller/Neo4jController.java
+++ BackEnd/src/main/java/xjtu/controller/Neo4jController.java
@@ -18,7 +18,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 @RestController
-@RequestMapping("/neo4j")
+@RequestMapping("/graph")
 public class Neo4jController {
     @Autowired
     private Neo4jService neo4jService;
